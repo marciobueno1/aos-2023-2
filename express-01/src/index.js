8d3456ab020440f372e41d870ae0e53a467a5ae8
Marcio Bueno <marciobueno@gmail.com>
2023-08-21 22:37:42 +0000

    Projeto Node Minimalista

---
+++ express-01/src/index.js
@@ -0,0 +1,6 @@
+import 'dotenv/config';
+// require('dotenv').config({ path: '../.env' })
+
+// console.log('Hello Project.');
+console.log('Hello ever running Node.js project.');
+console.log('process.env.MY_SECRET', process.env.MY_SECRET);
