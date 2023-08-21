3f65ee5d494e4c4c7ef99e8624f44e53715a80e0
Marcio Bueno <marciobueno@gmail.com>
2023-08-21 23:02:05 +0000

    Primeira versão do Express

--- express-01/src/index.js
+++ express-01/src/index.js
@@ -1,6 +1,15 @@
 import 'dotenv/config';
-// require('dotenv').config({ path: '../.env' })
+import express from 'express';
 
-// console.log('Hello Project.');
 console.log('Hello ever running Node.js project.');
 console.log('process.env.MY_SECRET', process.env.MY_SECRET);
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Olá Mundo!");
+});
+
+app.listen(3000, () => {
+    console.log('Exemplo de app escutando a porta 3000');
+});
