83fd84ec0cee2d7c33e8873526904fb299319284
Marcio Bueno <marciobueno@gmail.com>
2023-09-14 22:15:22 +0000

    Melhor estruturação do código (subpastas model e routers)

--- express-01/src/index.js
+++ express-01/src/index.js
@@ -1,10 +1,12 @@
-import 'dotenv/config';
-import cors from 'cors';
-import express from 'express';
-import { v4 as uuidv4 } from 'uuid';
+import "dotenv/config";
+import cors from "cors";
+import express from "express";
 
-console.log('Hello ever running Node.js project.');
-console.log('process.env.MY_SECRET', process.env.MY_SECRET);
+import models from "./models";
+import routes from "./routes";
+
+console.log("Hello ever running Node.js project.");
+console.log("process.env.MY_SECRET", process.env.MY_SECRET);
 
 const app = express();
 
@@ -13,104 +15,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use((req, res, next) => {
-  req.me = users[1];
+  req.context = {
+    models,
+    me: models.users[1],
+  };
   next();
 });
 
+app.use("/session", routes.session);
+app.use("/users", routes.user);
+app.use("/messages", routes.message);
 
-app.get('/', (req, res) => {
-    res.send("Hello World!");
-});
-
-let users = {
-  1: {
-    id: '1',
-    username: 'Robin Wieruch',
-  },
-  2: {
-    id: '2',
-    username: 'Dave Davids',
-  },
-};
-
-let messages = {
-  1: {
-    id: '1',
-    text: 'Hello World',
-    userId: '1',
-  },
-  2: {
-    id: '2',
-    text: 'By World',
-    userId: '2',
-  },
-};
-
-// USERS
-
-app.get('/users', (req, res) => {
-  return res.send(Object.values(users));
-});
-
-app.get('/users/:userId', (req, res) => {
-  return res.send(users[req.params.userId]);
-});
-
-app.post('/users', (req, res) => {
-  return res.send(`POST HTTP method on user resource: ${++count}\n`);
-});
-
-app.put('/users/:userId', (req, res) => {
-  return res.send(`PUT HTTP method on user/${req.params.userId} resource: ${++count}\n`);
-});
-
-app.delete('/users/:userId', (req, res) => {
-  return res.send(`DELETE HTTP method on user/${req.params.userId} resource: ${++count}\n`);
-});
-
-
-// MESSAGES
-
-app.get('/messages', (req, res) => {
-  return res.send(Object.values(messages));
-});
-
-app.get('/messages/:messageId', (req, res) => {
-  return res.send(messages[req.params.messageId]);
-});
-
-app.post('/messages', (req, res) => {
-  const id = uuidv4();
-  const message = {
-    id,
-    text: req.body.text,
-    userId: req.me.id,
-  };
-
-  messages[id] = message;
-
-  return res.send(message);
-});
-
-// app.delete('/messages/:messageId', (req, res) => {
-//   const {
-//     [req.params.messageId]: message,
-//     ...otherMessages
-//   } = messages;
-
-//   messages = otherMessages;
-
-//   return res.send(message);
-// });
-
-app.delete('/messages/:messageId', (req, res) => {
-  const message = messages[req.params.messageId];
-  delete messages[req.params.messageId];
-
-  return res.send(message);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () =>
-  console.log(`Example app listening on port ${port}!`),
-);
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
---
+++ express-01/src/routes/user.js
@@ -0,0 +1,29 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/", (req, res) => {
+  return res.send(Object.values(req.context.models.users));
+});
+
+router.get("/:userId", (req, res) => {
+  return res.send(req.context.models.users[req.params.userId]);
+});
+
+router.post("/", (req, res) => {
+  return res.send(`POST HTTP method on user resource: ${++count}\n`);
+});
+
+router.put("/:userId", (req, res) => {
+  return res.send(
+    `PUT HTTP method on user/${req.params.userId} resource: ${++count}\n`
+  );
+});
+
+router.delete("/:userId", (req, res) => {
+  return res.send(
+    `DELETE HTTP method on user/${req.params.userId} resource: ${++count}\n`
+  );
+});
+
+export default router;
