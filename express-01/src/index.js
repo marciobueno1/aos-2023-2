6330982dcebff77effcd91b2fc24095ef6d7a579
Marcio Bueno <marciobueno@gmail.com>
2023-09-21 22:03:48 +0000

    alterada as rotas para utilizar o sequelize

--- express-01/src/index.js
+++ express-01/src/index.js
@@ -14,10 +14,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use((req, res, next) => {
+app.use(async (req, res, next) => {
   req.context = {
     models,
-    // me: models.users[1],
+    me: await models.User.findByLogin("rwieruch"),
   };
   next();
 });
